d95baf9463a9a0eb0a4493f85edc26350bae842b
Ratthapon Sananaur <64584986+poomrtp@users.noreply.github.com>
2020-12-19 01:02:37 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,7 @@
         <b-navbar-brand href="#">
           <img src="../src/assets/pinterest.png" />
         </b-navbar-brand>
-
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav size="lg">
             <a href="#"><button class="btn btn-light me-2 rounded-pill" type="button">Home</button></a>
@@ -17,15 +15,18 @@
             v-model="topic" v-on:keyup.enter="searchUnsplash(topic)"></b-form-input>
           <!-- Right aligned nav items -->
           
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown text="lang" right>
+          <b-navbar-nav size="lg">
+            <b-nav-item-dropdown class="btn btn-light rounded-pill">
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <b-icon icon="globe2" aria-hidden="true" ></b-icon>
+              </template>
               <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
+              <b-dropdown-item href="#">DE</b-dropdown-item>
+              <b-dropdown-item href="#">JP</b-dropdown-item>
+              <b-dropdown-item href="#">CH</b-dropdown-item>
             </b-nav-item-dropdown>
-
-            <b-nav-item-dropdown right>
+            <b-nav-item-dropdown right class="btn btn-light rounded-pill">
               <!-- Using 'button-content' slot -->
               <template #button-content>
                 User
@@ -68,7 +69,6 @@ export default {
   }),
   methods: {
     searchUnsplash(topic){
-      console.log(topic)
       this.images = []
       axios.get(`https://api.unsplash.com/search/photos?query=${topic}&per_page=30`,{
         headers: {
